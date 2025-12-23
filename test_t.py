55557ff34a2d16cca9ad474181a51dafa3d83612
rakhi150 <rakhi150.ece@gmail.com>
2025-12-23 13:49:13 +0600

    initial commit

---
+++ test_t.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("My First Web App")
+
+name = st.text_input("Enter your name:")
+
+if st.button("Greet Me"):
+    st.write(f"Hello, {name}! 👋")
+
+numbers = st.slider("Pick a number", 1, 100, 50)
+st.write("You selected:", numbers)
