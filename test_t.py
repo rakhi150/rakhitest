4718ebc7084f4d7c18ba9e88b2cf2dbc2474d5ca
rakhi150 <rakhi150.ece@gmail.com>
2026-01-14 12:11:36 +0600

    Update test_t.py

--- test_t.py
+++ test_t.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
 st.title("My First Web App")
+st.subheader("Iam so excited")
 
 name = st.text_input("Enter your name:")
+
 
 if st.button("Greet Me"):
     st.write(f"Hello, {name}! 👋")
 
 numbers = st.slider("Pick a number", 1, 100, 50)
+
 st.write("You selected:", numbers)
